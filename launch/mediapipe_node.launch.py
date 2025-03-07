4eb9d452488b5e08bbbdbeefa76c1c52cd0ce5c9
gustavo-fardo <gustavo.fardo@hotmail.com>
2025-03-07 12:04:57 -0300

    launch working

--- launch/mediapipe_node.launch.py
+++ launch/mediapipe_node.launch.py
@@ -5,15 +5,18 @@ def generate_launch_description():
     return launch.LaunchDescription([
         launch_ros.actions.Node(
             package='mediapipe_track',
-            executable='mediapipe_node.py',
+            executable='mediapipe_node',
             name='mediapipe_node',
             output='screen',
             parameters=[
                 {'topic_namespace': '/utbots/vision'},
+                {'model_path': ''}, 
                 {'num_poses': 1},
                 {'detection_conf': 0.5},
                 {'presence_conf': 0.5},
                 {'track_conf': 0.5},
+                {'draw_skeleton_img:': True},
+                {'calculate_torso_point': True},
                 {'segmentation_mask': False},
                 {'rgb_topic': '/camera/rgb/image_color'}
             ]
